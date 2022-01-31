a143d637ca95348e6357c4fce5257e98d434df9c
CecyF92 <98528087+CecyF92@users.noreply.github.com>
2022-01-31 20:31:57 +0100

    Tolto il salvataggio e dichiarato nuovi eventi

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { Detenuto, Guardia, Type } from './types/global.d'
 import { getUtenti } from './mappatura'
-import { selModalita, selValore } from './dichiarazioni'
+import { btn, rimuovi, selModalita, selValore } from './dichiarazioni'
 import { cambio, detenuto, guardia, init } from './grafica'
+import { salva } from './salvataggio'
+import { remove } from './rimozione'
+
+init()
+
 
 window.addEventListener('load', function () {
-    init()
     const rigo = window.document.getElementById(
         'rigo'
     ) as HTMLTableSectionElement
@@ -13,6 +17,7 @@ window.addEventListener('load', function () {
     for (const guardia of guardie) {
         rigo.innerHTML += `
             <tr>
+                <td>${guardia.id}</td>
                 <td>${guardia.nome}</td>
                 <td>${guardia.cognome}</td>
                 <td>${guardia.eta}</td>
@@ -21,26 +26,26 @@ window.addEventListener('load', function () {
             guardia.data_assunzione?.getMonth() + 1
         }/${guardia.data_assunzione?.getFullYear()}</td>
                 <td>${guardia.descrizione}</td>
+                <td><button id="rimuovi" type="button" class="btn btn-outline-danger")">X</button></td>
             </tr>
            `
     }
 })
 
-selValore.addEventListener('change', cambio)
-
-selModalita.addEventListener('change', function () {
+export function visualizzazione() {
     let tipo: Type = selModalita.value as Type
-    console.log(tipo)
-    const rigo = window.document.getElementById(
-        'rigo'
-    ) as HTMLTableSectionElement
-    rigo.innerHTML = ''
     if (tipo === 'detenuto') {
         detenuto()
+        const rigo = window.document.getElementById(
+            'rigo'
+        ) as HTMLTableSectionElement
+        rigo.innerHTML = ''
         const detenuti = getUtenti('detenuto') as Detenuto[]
+        let i = 0
         for (const detenuto of detenuti) {
             rigo.innerHTML += `
                 <tr>
+                    <td>${detenuto.id}</td>
                     <td>${detenuto.nome}</td>
                     <td>${detenuto.cognome}</td>
                     <td>${detenuto.eta}</td>
@@ -55,22 +60,37 @@ selModalita.addEventListener('change', function () {
                     <td>${detenuto.crimine}</td>
                     <td>${detenuto.evaso}</td>
                     <td>${detenuto.deceduto}</td>
+                    <td><button id="rimuovi" type="button" class="btn btn-outline-danger")">X</button></td>
                 </tr>`
         }
     } else {
         guardia()
+        const rigo = window.document.getElementById(
+            'rigo'
+        ) as HTMLTableSectionElement
+        rigo.innerHTML = ''
         const guardie = getUtenti('guardia') as Guardia[]
         for (const guardia of guardie) {
             rigo.innerHTML += `
             <tr>
+                <td>${guardia.id}</td>
                 <td>${guardia.nome}</td>
                 <td>${guardia.cognome}</td>
                 <td>${guardia.eta}</td>
                 <td>${guardia.tipo}</td>
                 <td>${guardia.data_assunzione?.getDate()}/${guardia.data_assunzione?.getMonth()}/${guardia.data_assunzione?.getFullYear()}</td>
                 <td>${guardia.descrizione}</td>
+                <td><button id="rimuovi" type="button" class="btn btn-outline-danger")">X</button></td>
             </tr>
            `
         }
     }
-})
+}
+
+
+selValore.addEventListener('change', cambio)
+selModalita.addEventListener('change', visualizzazione)
+btn.addEventListener('click', salva)
+rimuovi.addEventListener('click', remove)
+
+
