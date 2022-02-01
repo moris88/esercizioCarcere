86d235c56749c05458bc6dbd9af3b23a38d17c77
CecyF92 <98528087+CecyF92@users.noreply.github.com>
2022-02-01 18:49:28 +0100

    Dichiarato ID e btnRemove

--- src/dichiarazioni.ts
+++ src/dichiarazioni.ts
@@ -14,4 +14,5 @@ export const divContenitore2 = window.document.querySelector(
     '.container2'
 ) as HTMLDivElement
 export const btn = window.document.getElementById('salva') as HTMLButtonElement
-export const rimuovi = window.document.getElementById('rimuovi') as HTMLButtonElement
+export const id = window.document.getElementById('id') as HTMLSelectElement
+export const btnRemove = window.document.getElementById('rimuovi') as HTMLButtonElement
