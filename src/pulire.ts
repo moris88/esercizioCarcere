f51e829e886a19a99901e1727c043b9fd3172094
CecyF92 <98528087+CecyF92@users.noreply.github.com>
2022-02-01 18:50:51 +0100

    File che pulisce il form

---
+++ src/pulire.ts
@@ -0,0 +1,39 @@
+import { removeUtente } from './mappatura';
+import { divContenitore2, divContenitore3, id, selModalita } from './dichiarazioni'
+import { cambio } from './grafica'
+import { Type } from './types/global';
+
+export function pulizia (){
+    divContenitore2.innerHTML = `
+    <h1>Nuovo Utente</h1>
+                    <table class="table">
+                        <tbody>
+                          <tr>
+                          <td></td>
+                            <th style="text-align: left;">
+                              Chi vuoi inserire: 
+                                <select name="tipo" id="tipo">
+                                    <option value="scelta" selected>SELEZIONA...</option>
+                                     <option value="g">GUARDIA</option>
+                                     <option value="d">DETENUTO</option>
+                                 </select>
+                             </th>
+                             <td></td>
+                          </tr>
+                          <tr>
+                            <th>Nome</th>
+                            <th>Cognome</th>
+                            <th>Età</th>
+                          </tr>
+                          <tr>
+                            <td><input type="text" aria-label="Nome" class="form-control" id="nome"></td>
+                            <td><input type="text" aria-label="Cognome" class="form-control" id="cognome"></td>
+                            <td><input type="number" id="eta"></td>
+                          </tr>
+                        </tbody>
+                    </table>
+    `
+    divContenitore3.innerHTML = ``
+}
+
+
