9fb9a52581ab106de8fc2c1303ca0aaf523091aa
CecyF92 <98528087+CecyF92@users.noreply.github.com>
2022-02-02 15:41:45 +0100

    Dichiarazione delle variabili per le statistiche

--- src/dichiarazioni.ts
+++ src/dichiarazioni.ts
@@ -16,3 +16,7 @@ export const divContenitore2 = window.document.querySelector(
 export const btn = window.document.getElementById('salva') as HTMLButtonElement
 export const id = window.document.getElementById('id') as HTMLSelectElement
 export const btnRemove = window.document.getElementById('rimuovi') as HTMLButtonElement
+export const nG = window.document.getElementById('nG') as HTMLElement
+export const nD = window.document.getElementById('nD') as HTMLElement
+export const eD = window.document.getElementById('eD') as HTMLElement
+export const dD = window.document.getElementById('dD') as HTMLElement
