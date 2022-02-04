73fe2018a0e03477fdee25a4a1ef8d105aa26334
CecyF92 <98528087+CecyF92@users.noreply.github.com>
2022-02-04 17:59:10 +0100

    Sistemato il file per la grafica

--- src/grafica.ts
+++ src/grafica.ts
@@ -1,37 +1,50 @@
+import { divNuovoUtente } from './main'
 import { Campo, Detenuto, Guardia, Type } from './types/global'
 
-export function cambiaVisualizzazioneUtente(tipo: Type, divElement: HTMLDivElement) {
+export function cambiaVisualizzazioneUtente(
+    tipo: Type,
+    divElement: HTMLDivElement
+) {
     if (tipo === 'guardia') {
         dati_guardia(divElement)
-    } else if (tipo ==='detenuto') {
+    } else if (tipo === 'detenuto') {
         dati_detenuto(divElement)
+    } else if (tipo === 'scelta') {
+        dati_Vuoti(divElement)
     }
 }
 
-export function cambioVisualizzazioneTabella(tipo: Type, divElement: HTMLDivElement) {
-    tipo === 'guardia' ? 
-    divElement.innerHTML = creazioneTabella([
-        'ID',
-        'Nome',
-        'Cognome',
-        'Età',
-        'Tipo',
-        'Data assunzione',
-        'Mansione',
-    ]) : 
-    divElement.innerHTML = creazioneTabella([
-        'ID',
-        'Nome',
-        'Cognome',
-        'Età',
-        'Tipo',
-        'Data carcerazione',
-        'Data scarcerazione',
-        'Pena',
-        'Crimine',
-        'Evaso',
-        'Deceduto',
-    ])    
+export function cambioVisualizzazioneTabella(
+    tipo: Type,
+    divElement: HTMLDivElement
+) {
+    tipo === 'guardia'
+        ? (divElement.innerHTML = creazioneTabella([
+              'ID',
+              'Nome',
+              'Cognome',
+              'Età',
+              'Tipo',
+              'Data assunzione',
+              'Mansione',
+          ]))
+        : (divElement.innerHTML = creazioneTabella([
+              'ID',
+              'Nome',
+              'Cognome',
+              'Età',
+              'Tipo',
+              'Data carcerazione',
+              'Data scarcerazione',
+              'Pena',
+              'Crimine',
+              'Evaso',
+              'Deceduto',
+          ]))
+}
+
+const dati_Vuoti = (divElement: HTMLDivElement) => {
+    divElement.innerHTML = ``
 }
 
 const dati_guardia = (divElement: HTMLDivElement) => {
@@ -105,10 +118,13 @@ function creazioneTabella(listaCampi: Campo[]) {
     return output
 }
 
-export function inserisciTupla(utenti: Detenuto[]|Guardia[], divElement: HTMLElement){
+export function inserisciTupla(
+    utenti: Detenuto[] | Guardia[],
+    divElement: HTMLElement
+) {
     divElement.innerHTML = ``
-    for(let utente of utenti){
-        if(utente.tipo==='detenuto'){
+    for (let utente of utenti) {
+        if (utente.tipo === 'detenuto') {
             const detenuto = utente as Detenuto
             divElement.innerHTML += `
             <tr>
@@ -117,14 +133,19 @@ export function inserisciTupla(utenti: Detenuto[]|Guardia[], divElement: HTMLEle
                 <td>${detenuto.cognome}</td>
                 <td>${detenuto.eta}</td>
                 <td>${detenuto.tipo}</td>
-                <td>${detenuto.data_carcerazione}</td>
-                <td>${detenuto.data_scarcerazione}</td>
+                <td>${detenuto.data_carcerazione.getDate()}/${
+                detenuto.data_carcerazione.getMonth() + 1
+            }/${detenuto.data_carcerazione.getFullYear()}</td>
+                <td>${detenuto.data_scarcerazione.getDate()}/${
+                detenuto.data_scarcerazione.getMonth() + 1
+            }/${detenuto.data_scarcerazione.getFullYear()}</td>
                 <td>${detenuto.pena}</td>
                 <td>${detenuto.crimine}</td>
                 <td>${detenuto.evaso}</td>
                 <td>${detenuto.deceduto}</td>
+                <td><button type="button" class="btn btn-danger" id="btnRemove" onClick="removeUtente(${detenuto.id, 'detenuto'})">X</button></td>
             </tr>`
-        }else{
+        } else {
             const guardia = utente as Guardia
             divElement.innerHTML += `
             <tr>
@@ -133,11 +154,79 @@ export function inserisciTupla(utenti: Detenuto[]|Guardia[], divElement: HTMLEle
                 <td>${guardia.cognome}</td>
                 <td>${guardia.eta}</td>
                 <td>${guardia.tipo}</td>
-                <td>${guardia.data_assunzione}</td>
+                <td>${guardia.data_assunzione.getDate()}/${
+                guardia.data_assunzione.getMonth() + 1
+            }/${guardia.data_assunzione.getFullYear()}</td>
                 <td>${guardia.descrizione}</td>
+                <td><button type="button" class="btn btn-danger" id="btnRemove" onClick="removeUtente(${guardia.id, 'guardia'})">X</button></td>
             </tr>`
         }
     }
+}
+
+export function pulizia() {
+    divNuovoUtente.innerHTML = `
+        <h1>Nuovo Utente</h1>
+        <form>
+          <div class="mb-3">
+            <label for="scelta" class="form-label">Chi vuoi inserire: </label>
+            <select name="tipo" id="scegliTipoUtente">
+                <option value="scelta"  class="form-control" selected>SELEZIONA...</option>
+                <option value="guardia">GUARDIA</option>
+                <option value="detenuto">DETENUTO</option>
+            </select>
+          </div>
+          <div class="p-3">
+            <div class="row">
+              <div class="col">
+                <label for="name" class="form-label">Nome</label>
+              </div>
+              <div class="col">
+                <label class="form-check-label" for="cognome">Cognome</label>
+              </div>
+              <div class="col">
+                <label class="form-check-label" for="eta">Età</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col">
+                <input type="text" aria-label="Nome" class="form-control" id="nome">
+              </div>
+              <div class="col">
+                <input type="text" aria-label="Cognome" class="form-control" id="cognome">
+              </div>
+              <div class="col">
+                <input type="number" class="form-control" id="eta">
+              </div>
+            </div>
+          </div>
+          <div class="mb-3 form-check" id="divDatiAggiuntiviUtente">
+        
+          </div>
+          <div class="container4"><button type="button" id="salva" class="btn btn-outline-secondary">Salva</button></div>
+        </form>
+        <hr>
+                <div class="divStatistiche">
+                  <div class="row">
+                    <div class="col">
+                      <h6>Le guardie sono: </h6>
+                      <h6>I detenuti sono: </h6>
+                    </div>
+                    <div class="col">
+                      <h4 id="nG"></h4>
+                      <h4 id="nD"></h4>
+                    </div>
+                    <div class="col">
+                      <h6>I detenuti evasi sono: </h6>
+                      <h6>I detenuti deceduti sono: </h6>
+                    </div>
+                    <div class="col">
+                      <h4 id="eD"></h4>
+                      <h4 id="dD"></h4>
+                    </div>
+                  </div>
+                </div>
+    `
 }
 
 /*function inserisci(testo: string){
