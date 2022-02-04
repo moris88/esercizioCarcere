c37d2312c62369a6ccc520cc85a72240d8dda661
CecyF92 <98528087+CecyF92@users.noreply.github.com>
2022-02-04 17:58:38 +0100

    Sistemato il collegamento con il database

--- src/database.ts
+++ src/database.ts
@@ -1,65 +1,80 @@
+import { pulizia } from './grafica'
+import { dD, eD, scegliTipoUtente, visualizzaTabellaUtente } from './main'
 import { nG, nD, fascicolo } from './mappatura'
-import { pulizia } from './pulire'
+import { decesso, evasione, numero } from './statistiche'
 import { Detenuto, Guardia, Type } from './types/global'
 
 export function salva() {
-   /*  let tipo = scegliTipoUtente.value
-    let idG = guardia.length + nG
-    let idD = detenuto.length + nD
+    const detenuto = getUtenti('detenuto') as Detenuto[]
+    const guardia = getUtenti('guardia') as Guardia[]
+    let tipo = scegliTipoUtente.value
+    let idG = nG
+    let idD = nD
     let nome = document.getElementById('nome') as HTMLInputElement
     let cognome = document.getElementById('cognome') as HTMLInputElement
     let eta = document.getElementById('eta') as HTMLInputElement
-    if (tipo === 'g') {
-        let data_assunzione = document.getElementById(
-            'data_assunzione'
-        ) as HTMLDataElement
-        let descrizione = document.getElementById(
-            'mansione'
-        ) as HTMLInputElement
-        idG++
-        let persona: Guardia = {
-            id: idG,
-            nome: nome.value,
-            cognome: cognome.value,
-            eta: Number(eta.value),
-            tipo: 'guardia',
-            data_assunzione: new Date(data_assunzione.value),
-            descrizione: descrizione.value,
+    if (nome.value === '') {
+        alert('Attenzione! ci sono dei campi vuoti!')
+    } else {
+        if (tipo === 'guardia') {
+            let data_assunzione = document.getElementById(
+                'data_assunzione'
+            ) as HTMLDataElement
+            let descrizione = document.getElementById(
+                'mansione'
+            ) as HTMLInputElement
+            idG++
+            let persona: Guardia = {
+                id: idG,
+                nome: nome.value,
+                cognome: cognome.value,
+                eta: Number(eta.value),
+                tipo: 'guardia',
+                data_assunzione: new Date(data_assunzione.value),
+                descrizione: descrizione.value,
+            }
+            addUtente(persona)
+        } else if (tipo === 'detenuto') {
+            let data_carcerazione = document.getElementById(
+                'data_carcerazione'
+            ) as HTMLDataElement
+            let data_c = new Date(data_carcerazione.value)
+            let data_scarcerazione = document.getElementById(
+                'data_scarcerazione'
+            ) as HTMLDataElement
+            let data_s = new Date(data_scarcerazione.value)
+            let crimine = document.getElementById('crimine') as HTMLInputElement
+            let evaso = document.getElementById(
+                'checkEvaso'
+            ) as HTMLInputElement
+            let deceduto = document.getElementById(
+                'checkDeceduto'
+            ) as HTMLInputElement
+            idD++
+            let persona: Detenuto = {
+                id: idD,
+                nome: nome.value,
+                cognome: cognome.value,
+                eta: Number(eta.value),
+                tipo: 'detenuto',
+                data_carcerazione: new Date(data_carcerazione.value),
+                data_scarcerazione: new Date(data_scarcerazione.value),
+                pena:
+                    Number(data_s.getFullYear()) - Number(data_c.getFullYear()),
+                crimine: crimine.value,
+                evaso: evaso.checked,
+                deceduto: deceduto.checked,
+            }
+            addUtente(persona)
         }
-        addUtente(persona)
-    } else if (tipo === 'd') {
-        let data_carcerazione = document.getElementById(
-            'data_carcerazione'
-        ) as HTMLDataElement
-        let data_c = new Date(data_carcerazione.value)
-        let data_scarcerazione = document.getElementById(
-            'data_scarcerazione'
-        ) as HTMLDataElement
-        let data_s = new Date(data_scarcerazione.value)
-        let crimine = document.getElementById('crimine') as HTMLInputElement
-        let evaso = document.getElementById('checkEvaso') as HTMLInputElement
-        let deceduto = document.getElementById(
-            'checkDeceduto'
-        ) as HTMLInputElement
-        idD++
-        let persona: Detenuto = {
-            id: idD,
-            nome: nome.value,
-            cognome: cognome.value,
-            eta: Number(eta.value),
-            tipo: 'detenuto',
-            data_carcerazione: new Date(data_carcerazione.value),
-            data_scarcerazione: new Date(data_scarcerazione.value),
-            pena: Number(data_s.getFullYear()) - Number(data_c.getFullYear()),
-            crimine: crimine.value,
-            evaso: evaso.checked,
-            deceduto: deceduto.checked,
-        }
-        addUtente(persona)
+        visualizzaTabellaUtente()
+        pulizia()
+        numero(detenuto, guardia)
+        evasione(detenuto)
+        decesso(detenuto)
+        scegliTipoUtente.value = 'scelta'
     }
-    visualizzazione()
-    pulizia()
-    scegliTipoUtente.value = 'scelta' */
+    visualizzaTabellaUtente()
 }
 
 export const addUtente = (utente: Detenuto | Guardia) => {
@@ -70,11 +85,16 @@ export const addUtente = (utente: Detenuto | Guardia) => {
     }
 }
 
-export const removeUtente = (pos: number, tipo: Type) => {
+export function removeUtente(pos: number, tipo: Type) {
+    console.log('Sono qui')
     if (tipo === 'detenuto') {
+        console.log('Sto per cancellare un detenuto')
         fascicolo.detenuti.splice(pos - 1, 1)
+        console.log('Ho cancellato un detenuto')
     } else {
+        console.log('Sto per cancellare duna guardia')
         fascicolo.guardie.splice(pos - 1, 1)
+        console.log('Ho cancellato una guardia')
     }
 }
 
