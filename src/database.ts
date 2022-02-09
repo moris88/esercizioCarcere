ef5c5d8541eeb119e7d015d66c98163b2e0474fa
CecyF92 <98528087+CecyF92@users.noreply.github.com>
2022-02-09 18:27:49 +0100

    Eliminata la funzione salva e aggiunto i dati

--- src/database.ts
+++ src/database.ts
@@ -1,129 +1,156 @@
-import { Detenuto, Database, Guardia, Type } from './types/global'
+import { Detenuto, Fascicolo, Guardia, Type } from './types/global'
 
 /*
-    aggiungere la statisticam il database non gestisce la grafica
+    aggiungere la statistica il database non gestisce la grafica
     implementare il namespace come in grafica
     inserisci tutto qui in database (fascicolo e tutti i detenuti e guardie)
 */
-
-const fascicolo: Database = {
-    guardie: [],
-    detenuti: [],
-}
-
-//namespace
-
-export function inizializzaDatabase() {
-    //inserisce la mappatura
-    const detenuto1: Detenuto = {
-        id: 1,
-        nome: 'Francesco',
-        cognome: 'Totti',
-        eta: 45,
-        tipo: 'detenuto',
-        data_carcerazione: new Date('2018/10/25'),
-        data_scarcerazione: new Date('2038/10/25'),
-        pena: 20,
-        crimine: 'truffa ai danni dello stato',
-        evaso: false,
-        deceduto: false,
+namespace Database {
+    const fascicolo: Fascicolo = {
+        guardie: [],
+        detenuti: [],
     }
-    fascicolo.detenuti.push(detenuto1)
-}
 
-export function salva() {
-    const detenuto = getUtenti('detenuto') as Detenuto[]
-    const guardia = getUtenti('guardia') as Guardia[]
-    let tipo = scegliTipoUtente.value
-    let idG = nG
-    let idD = nD
-    let nome = document.getElementById('nome') as HTMLInputElement
-    let cognome = document.getElementById('cognome') as HTMLInputElement
-    let eta = document.getElementById('eta') as HTMLInputElement
-    if (nome.value === '' || cognome.value === '' ) {
-        alert('Attenzione! ci sono dei campi vuoti!')
-    } else {
-        if (tipo === 'guardia') {
-            let data_assunzione = document.getElementById(
-                'data_assunzione'
-            ) as HTMLDataElement
-            let descrizione = document.getElementById(
-                'mansione'
-            ) as HTMLInputElement
-            idG++
-            let persona: Guardia = {
-                id: idG,
-                nome: nome.value,
-                cognome: cognome.value,
-                eta: Number(eta.value),
-                tipo: 'guardia',
-                data_assunzione: new Date(data_assunzione.value),
-                descrizione: descrizione.value,
-            }
-            addUtente(persona)
-        } else if (tipo === 'detenuto') {
-            let data_carcerazione = document.getElementById(
-                'data_carcerazione'
-            ) as HTMLDataElement
-            let data_c = new Date(data_carcerazione.value)
-            let data_scarcerazione = document.getElementById(
-                'data_scarcerazione'
-            ) as HTMLDataElement
-            let data_s = new Date(data_scarcerazione.value)
-            let crimine = document.getElementById('crimine') as HTMLInputElement
-            let evaso = document.getElementById(
-                'checkEvaso'
-            ) as HTMLInputElement
-            let deceduto = document.getElementById(
-                'checkDeceduto'
-            ) as HTMLInputElement
-            idD++
-            let persona: Detenuto = {
-                id: idD,
-                nome: nome.value,
-                cognome: cognome.value,
-                eta: Number(eta.value),
-                tipo: 'detenuto',
-                data_carcerazione: new Date(data_carcerazione.value),
-                data_scarcerazione: new Date(data_scarcerazione.value),
-                pena:
-                    Number(data_s.getFullYear()) - Number(data_c.getFullYear()),
-                crimine: crimine.value,
-                evaso: evaso.checked,
-                deceduto: deceduto.checked,
-            }
-            addUtente(persona)
+    //namespace
+
+    export function inizializzaDatabase() {
+        //inserisce la mappatura
+        const detenuto1: Detenuto = {
+            id: 1,
+            nome: 'Francesco',
+            cognome: 'Totti',
+            eta: 45,
+            tipo: 'detenuto',
+            data_carcerazione: new Date('2018/10/25'),
+            data_scarcerazione: new Date('2038/10/25'),
+            pena: 20,
+            crimine: 'truffa ai danni dello stato',
+            evaso: false,
+            deceduto: false,
         }
-        visualizzaTabellaUtente()
-        pulizia()
-        numero(detenuto, guardia)
-        evasione(detenuto)
-        decesso(detenuto)
-        scegliTipoUtente.value = 'scelta'
+        fascicolo.detenuti.push(detenuto1)
+
+        const detenuto2: Detenuto = {
+            id: 2,
+            nome: 'Giacomo',
+            cognome: 'Pippo',
+            eta: 25,
+            tipo: 'detenuto',
+            data_carcerazione: new Date('2019/11/5'),
+            data_scarcerazione: new Date('2069/11/5'),
+            pena: 50,
+            crimine: 'omidicio',
+            evaso: true,
+            deceduto: false,
+        }
+
+        fascicolo.detenuti.push(detenuto2)
+
+        const guardia1: Guardia = {
+            id: 1,
+            nome: 'Maurizio',
+            cognome: 'Tolomeo',
+            eta: 34,
+            tipo: 'guardia',
+            data_assunzione: new Date('2020/5/17'),
+            descrizione: 'agente',
+        }
+
+        fascicolo.guardie.push(guardia1)
+
+        const guardia2: Guardia = {
+            id: 2,
+            nome: 'Giacomo',
+            cognome: 'Sardo',
+            eta: 32,
+            tipo: 'guardia',
+            data_assunzione: new Date('2018/1/1'),
+            descrizione: 'agente',
+        }
+
+        fascicolo.guardie.push(guardia2)
     }
-    visualizzaTabellaUtente()
+
+    /**
+     *
+     * @param utente
+     */
+    export const addUtente = (utente: Detenuto | Guardia) => {
+        if (utente.tipo === 'detenuto') {
+            fascicolo.detenuti.push(utente as Detenuto)
+        } else {
+            fascicolo.guardie.push(utente as Guardia)
+        }
+    }
+
+    /**
+     *
+     * @param pos
+     * @param tipo
+     */
+    export function removeUtente(pos: number, tipo: Type) {
+        if (tipo === 'detenuto') {
+            fascicolo.detenuti.splice(pos - 1, 1)
+        } else {
+            fascicolo.guardie.splice(pos - 1, 1)
+        }
+    }
+
+    /**
+     *
+     * @param tipo
+     * @returns
+     */
+    export const getUtenti = (tipo: Type) => {
+        if (tipo === 'detenuto') {
+            return fascicolo.detenuti as Detenuto[]
+        } else {
+            return fascicolo.guardie as Guardia[]
+        }
+    }
+
+    /**
+     *
+     * @param detenuti
+     * @param guardie
+     * @returns
+     */
+    export const numero = (detenuti: Detenuto[], guardie: Guardia[]) => {
+        return {
+            guardie: guardie.length,
+            detenuti: detenuti.length,
+        }
+    }
+
+    /**
+     *
+     * @param detenuti
+     * @returns
+     */
+    export const evasione = (detenuti: Detenuto[]) => {
+        let numEvaso = 0
+        for (const detenuto of detenuti) {
+            if (detenuto.evaso === true) {
+                numEvaso++
+            }
+        }
+        return { numEvaso }
+    }
+
+    /**
+     *
+     * @param detenuti
+     * @returns
+     */
+    export const decesso = (detenuti: Detenuto[]) => {
+        let numDecesso = 0
+        for (const detenuto of detenuti) {
+            if (detenuto.deceduto === true) {
+                numDecesso++
+            }
+        }
+        return { numDecesso }
+    }
 }
 
-export const addUtente = (utente: Detenuto | Guardia) => {
-    if (utente.tipo === 'detenuto') {
-        fascicolo.detenuti.push(utente as Detenuto)
-    } else {
-        fascicolo.guardie.push(utente as Guardia)
-    }
-}
-
-export function removeUtente(pos: number, tipo: Type) {
-    if (tipo === 'detenuto') {
-        fascicolo.detenuti.splice(pos - 1, 1)
-    } else {
-        fascicolo.guardie.splice(pos - 1, 1)
-    }
-}
-
-export const getUtenti = (tipo: Type) => {
-    if (tipo === 'detenuto') {
-        return fascicolo.detenuti as Detenuto[]
-    } else {
-        return fascicolo.guardie as Guardia[]
-    }
-}
+export default Database
